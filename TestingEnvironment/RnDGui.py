488b8aa09a167bf6eed53f5a91c868662f246660
Matthew Volpe <69683445+mVolpe94@users.noreply.github.com>
2021-02-02 08:37:19 -0500

    Update RnDGui.py

--- TestingEnvironment/RnDGui.py
+++ TestingEnvironment/RnDGui.py
@@ -1,29 +1,30 @@
-#import RPi.GPIO as gpio
+import RPi.GPIO as gpio
 import time, os, sys
 import mainprocess
 from tkinter import *
 from tkinter import ttk
 
 augers = 5
-heat_exg_blower = 13
+starter = 6
+convection_blower = 13
 combustion_blower = 21
 room_air_sensor = 19
 outgoing_air_sensor = 26
 flame_sensor = 20
 
 #pin setup
-# gpio.setup(augers, gpio.OUT)
-# gpio.setup(heat_exg_blower, gpio.OUT)
-# gpio.setup(combustion_blower, gpio.OUT)
-# gpio.setup(room_air_sensor, gpio.IN)
-# gpio.setup(outgoing_air_sensor, gpio.IN)
-# gpio.setup(flame_sensor, gpio.IN)
+gpio.setup(augers, gpio.OUT)
+gpio.setup(convection_blower, gpio.OUT)
+gpio.setup(combustion_blower, gpio.OUT)
+gpio.setup(room_air_sensor, gpio.IN)
+gpio.setup(outgoing_air_sensor, gpio.IN)
+gpio.setup(flame_sensor, gpio.IN)
 
 
 
 class RndGui:
     def __init__(self, root):
-        root.title("PELLET STOVE RnD")
+        root.title("PELLET STOVE R+D")
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
@@ -45,64 +46,58 @@ class RndGui:
         self.auger_time = ttk.Entry(settings_frame, width=11, textvariable=auger_run_time)
         self.auger_time.grid(column=1, row=0, padx=5, sticky=(W,E))
 
-        self.auger_button = ttk.Button(settings_frame, text="Run", command=None)
+        self.auger_button = ttk.Button(settings_frame, text="Run", command=lambda: self.auger_timer(auger_run_time.get()))
         self.auger_button.grid(column=2, row=0, padx=5, sticky=W)
 
-        auger_run_button = ttk.Button(settings_frame, text="On", command=None)
-        auger_run_button.grid(column=1, row=1, padx=5, sticky=W)
+        auger_off_button = ttk.Button(settings_frame, text="Off", command=lambda: gpio.output(augers, False))
+        auger_off_button.grid(column=1, row=1, padx=5, sticky=W)
 
-        auger_off_button = ttk.Button(settings_frame, text="Off", command=None)
-        auger_off_button.grid(column=2, row=1, padx=5, sticky=W)
+        auger_on_button = ttk.Button(settings_frame, text="On", command=lambda: gpio.output(augers, True))
+        auger_on_button.grid(column=2, row=1, padx=5, sticky=W)
 
         ttk.Separator(settings_frame, orient=HORIZONTAL).grid(row=2, pady=8, columnspan=3, sticky=(E,W))
 
         #Combustion Blower Test
-        combustion_blower_label = ttk.Label(settings_frame, text="Combustion Blower Speed:")
+        combustion_blower_label = ttk.Label(settings_frame, text="Combustion Blower:")
         combustion_blower_label.grid(column=0, row=3, sticky=W)
 
-        combustion_blower_off = ttk.Button(settings_frame, text="Off", command=None)
+        combustion_blower_off = ttk.Button(settings_frame, text="Off", command=lambda: gpio.output(combustion_blower, False))
         combustion_blower_off.grid(column=1, row=3, padx=5, sticky=W)
 
-        combustion_blower_low = ttk.Button(settings_frame, text="Low", command=None)
-        combustion_blower_low.grid(column=2, row=3, padx=5, sticky=W)
+        combustion_blower_on = ttk.Button(settings_frame, text="On", command=lambda: gpio.output(combustion_blower, True))
+        combustion_blower_on.grid(column=2, row=3, padx=5, sticky=W)
 
-        combustion_blower_med = ttk.Button(settings_frame, text="Medium", command=None)
-        combustion_blower_med.grid(column=1, row=4, padx=5, sticky=W)
-
-        combustion_blower_high = ttk.Button(settings_frame, text="High", command=None)
-        combustion_blower_high.grid(column=2, row=4, padx=5, sticky=W)
-
-        ttk.Separator(settings_frame, orient=HORIZONTAL).grid(row=5, pady=8, columnspan=3, sticky=(E,W))
+        ttk.Separator(settings_frame, orient=HORIZONTAL).grid(row=4, pady=8, columnspan=3, sticky=(E,W))
 
         #Starter Test
         starter_run_time = IntVar()
         starter_label = ttk.Label(settings_frame, text="Starter Time Test:")
-        starter_label.grid(column=0, row=6, sticky=W)
+        starter_label.grid(column=0, row=5, sticky=W)
 
         starter_time = ttk.Entry(settings_frame, width=11, textvariable=starter_run_time)
-        starter_time.grid(column=1, row=6, padx=5, sticky=(W,E))
+        starter_time.grid(column=1, row=5, padx=5, sticky=(W,E))
 
-        starter_button = ttk.Button(settings_frame, text="Run", command=None)
-        starter_button.grid(column=2, row=6, padx=5, sticky=W)
+        starter_button = ttk.Button(settings_frame, text="Run", command=lambda: self.starter_timer(starter_run_time.get()))
+        starter_button.grid(column=2, row=5, padx=5, sticky=W)
+
+        starter_off_button = ttk.Button(settings_frame, text="Off", command=lambda: gpio.output(starter, False))
+        starter_off_button.grid(column=1, row=6, padx=5, sticky=W)
+
+        starter_on_button = ttk.Button(settings_frame, text="On", command=lambda: gpio.output(starter, True))
+        starter_on_button.grid(column=2, row=6, padx=5, sticky=W)
 
         ttk.Separator(settings_frame, orient=HORIZONTAL).grid(row=7, pady=8, columnspan=3, sticky=(E,W))
 
 
         #Convection Blower Test
-        convection_blower_label = ttk.Label(settings_frame, text="Convection Blower Speed:")
+        convection_blower_label = ttk.Label(settings_frame, text="Convection Blower:")
         convection_blower_label.grid(column=0, row=8, sticky=W)
 
-        convection_blower_off = ttk.Button(settings_frame, text="Off", command=None)
+        convection_blower_off = ttk.Button(settings_frame, text="Off", command=lambda: gpio.output(convection_blower, False))
         convection_blower_off.grid(column=1, row=8, padx=5, sticky=W)
 
-        convection_blower_low = ttk.Button(settings_frame, text="Low", command=None)
-        convection_blower_low.grid(column=2, row=8, padx=5, sticky=W)
-
-        convection_blower_med = ttk.Button(settings_frame, text="Medium", command=None)
-        convection_blower_med.grid(column=1, row=9, padx=5, sticky=W)
-
-        convection_blower_high = ttk.Button(settings_frame, text="High", command=None)
-        convection_blower_high.grid(column=2, row=9, padx=5, sticky=W)
+        convection_blower_on = ttk.Button(settings_frame, text="On", command=lambda: gpio.output(convection_blower, True))
+        convection_blower_on.grid(column=2, row=8, padx=5, sticky=W)
 
 
         #Read Room Temp
@@ -112,6 +107,22 @@ class RndGui:
 
         read_temp_button = ttk.Button(stats_frame, text="Read Temp", command=lambda: room_air_temp.set(read_temp()))
         read_temp_button.grid(column=1, row=0, sticky=W, padx=5)
+
+
+    def auger_timer(self, sec):
+            start_time = time.time()
+            gpio.output(augers, True)
+            if start_time > time.time() + sec:
+                gpio.output(augers, False)
+
+    
+    def starter_timer(self, sec):
+            start_time = time.time()
+            gpio.output(starter, True)
+            if start_time > time.time() + sec:
+                gpio.output(starter, False)
+
+
 
 def read_temp():
     temp_file = open("TestingEnvironment/temptest.txt")
